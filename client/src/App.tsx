b0557cda7cb85e2624f8f1956e274112bd0ee90a
Deli Soetiawan <deli.soetiawan@gmail.com>
2024-06-16 16:32:12 +0700

    feat(client): setup initial states

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 
 function App() {
+  const [issues, setIssues] = useState([]);
+  const [issueDetail, setIssueDetail] = useState(null);
+  const [openForm, setOpenForm] = useState(false);
+  const [openDelete, setOpenDelete] = useState(false);
+  const [payloadData, setPayloadData] = useState(null);
+  const [deleteId, setDeleteId] = useState(0);
+  const serverUrl = 'http://localhost:9000/api/v1';
+
   return (
 
   );
