62e7f3cc4b696fafd6dc9af23f395e8833fe28cc
Deli Soetiawan <deli.soetiawan@gmail.com>
2024-06-16 17:36:50 +0700

    fix(client): rename label from name to title

--- client/src/components/FormInput.tsx
+++ client/src/components/FormInput.tsx
@@ -53,7 +53,7 @@ export function FormInput({
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="title" className="text-right">
-              Name
+              Title
             </Label>
             <Input
               id="title"
